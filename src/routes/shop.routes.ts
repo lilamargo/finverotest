15b6fc60c74277ea153d0496d4c9fc9141bbd4df
Liliana <89320049+lilamargo@users.noreply.github.com>
2022-02-03 13:32:26 -0600

    feature: Readme y shop CRUD

--- src/routes/shop.routes.ts
+++ src/routes/shop.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 const router = Router();
 
-import { getShop } from "../controllers/shop.controller";
+import {
+  getShop,
+  createShop,
+  getShopId,
+  deleteShop,
+  updateShop,
+} from "../controllers/shop.controller";
 
-router.route("/").get(getShop);
-// router.route("/:userId").get(getUserId).delete(deleteUser).put(updateUser);
+router.route("/").get(getShop).post(createShop);
+router.route("/:shopId").get(getShopId).delete(deleteShop).put(updateShop);
 
 export default router;
